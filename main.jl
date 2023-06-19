082136866fc58be957aeb251f24dbc22cfa7696f
Rafael Lucena Germano <rafaellg79@hotmail.com>
2023-06-19 13:34:07 -0300

    Altered render to return an Array{RGB} instead of saving the image.

--- main.jl
+++ main.jl
@@ -4,7 +4,7 @@ include("scenes.jl")
 """
     render([F=Float32], world::Vector{<:Hittable}; kwargs...)
 
-Ray trace `world` and saves the resulting image into a file.
+Ray trace `world` and returns an `Array{RGB{F}}` with the rendered image.
 
 #Arguments:
 - `width` : width in pixels 
@@ -12,7 +12,6 @@ Ray trace `world` and saves the resulting image into a file.
 - `camera` : `Camera` object viewing the world
 - `background` : a `Vec3` with the background color by default it's sky blue if no emissive object exists, otherwise it's black
 - `pattern` : matrix `[x_0 ... x_n; y_0 ... y_n]` with the horizontal and vertical offsets of the ray direction
-- `output_filename` : file in which the resulting image will be stored with format inferred by file extension (see [`save`](@ref) for supported formats).
 - `gpu` : set true to use CUDA.jl implementation
 
 Note: the CUDA.jl implementation does not support all CPU features.
@@ -37,8 +36,7 @@ function render(::Type{F}, world::Vector{<:Hittable};
         draw!(img, world, camera; background = background, pattern = pattern)
     end
     
-    save(output_filename, clamp01nan.(img))
-    display("Saved image in $output_filename")
+    return img
 end
 
 render(world::Vector{<:Hittable}; kwargs...) = render(Float32, world; kwargs...)
